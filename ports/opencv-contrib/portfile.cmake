9f7982e02a870432a4b8074c7af9e1935bee8bd5
HormigaDias <hormiga@gmx.de>
2020-03-08 01:20:26 +0100

    Update portfile.cmake

--- ports/opencv-contrib/portfile.cmake
+++ ports/opencv-contrib/portfile.cmake
@@ -1,7 +1,7 @@
 include(vcpkg_common_functions)
 
 vcpkg_from_github(
-    OUT_SOURCE_PATH modules/aruco
+    OUT_SOURCE_PATH SOURCE_PATH
     REPO opencv/opencv_contrib
     REF 4.2.0
     SHA512 073a6d12cbd95a25a67462924eab0967057ef74f1e9397afd196e5bc651b0192b6670165b3bc30fab0eb0a8bbae01457e2245e44ff88a6947e69381fe0887a9b
@@ -9,7 +9,7 @@ vcpkg_from_github(
 )
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH ${SOURCE_PATH}/modules/aruco
     PREFER_NINJA
 )
 vcpkg_install_cmake()
