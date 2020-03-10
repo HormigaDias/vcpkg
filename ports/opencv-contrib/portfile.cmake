af3a5a14e523c23b0ed5e80bc0130bc64a994f72
HormigaDias <hormiga@gmx.de>
2020-03-11 00:29:08 +0100

    Update portfile.cmake

--- ports/opencv-contrib/portfile.cmake
+++ ports/opencv-contrib/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO opencv/opencv_contrib
     REF 3.4.1
-    SHA512 073a6d12cbd95a25a67462924eab0967057ef74f1e9397afd196e5bc651b0192b6670165b3bc30fab0eb0a8bbae01457e2245e44ff88a6947e69381fe0887a9b
+    SHA512 431dfba0f413071d7faa18bc6e6f5e4f015285e2cc730c5dd69b2a4d6aa4250b7e0bcb1814ac6f06f5c76f103aea1f93f72f32aee6bc0cd7ddacdaf1f40075c1
     HEAD_REF master
 )
 
